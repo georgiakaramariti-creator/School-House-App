ddefebb50e369d6897841061ccd38b591d2abfd1
georgiakaramariti-creator <georgia.karamariti@gmail.com>
2026-08-24 22:40:27 +0300

    Bump School House service worker cache to v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='school-house-v2';
-const ASSETS=['./','./index.html','./styles.css?v=2','./app.js?v=2','./manifest.webmanifest?v=2','./icon.svg'];
+const CACHE='school-house-v3';
+const ASSETS=['./','./index.html','./styles.css?v=3','./app.js?v=3','./manifest.webmanifest?v=3','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
